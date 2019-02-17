91c8e4c63b16a5a3fa3038b489c59735f54f8185
Heet1996 <heet1885@gmail.com>
2019-02-17 14:27:53 +0530

    added new post

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
+
 
 class NewPost extends Component {
     state = {
@@ -8,7 +10,17 @@ class NewPost extends Component {
         content: '',
         author: 'Max'
     }
-
+    postHandler=()=>{
+        
+            let data= {
+                title:this.state.title,
+                content:this.state.content,
+                author:this.state.author,
+            };
+            axios.post('https://jsonplaceholder.typicode.com/posts',data)
+                .then((response)=>console.log(response));
+        
+    }
     render () {
         return (
             <div className="NewPost">
@@ -22,7 +34,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postHandler}>Add Post</button>
             </div>
         );
     }
